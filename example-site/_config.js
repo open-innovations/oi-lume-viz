a56f5af7c52aa3713fab8c21b567c2ca6c07086d
Giles Dring <giles@dringtech.com>
2022-11-08 11:13:16 +0000

    Load autodependency to sample site

--- example-site/_config.js
+++ example-site/_config.js
@@ -2,6 +2,7 @@ import * as path from 'std/path/mod.ts';
 import lume from "lume/mod.ts";
 
 import charts from '../mod.ts';
+import autoDependency from 'https://cdn.jsdelivr.net/gh/open-innovations/oi-lume-utils@0.2.0-pre/processors/auto-dependency.ts';
 
 const site = lume({
   src: '.',
@@ -10,9 +11,11 @@ const site = lume({
 site.loadAssets([".css"]);
 
 site.use(charts({
-  assetPath: 'assets/oi',
+  assetPath: '/assets/oi',
   componentNamespace: 'oi.charts',
 }));
+
+site.process(['.html'], autoDependency);
 
 console.log(path.resolve('README.md'));
 site.remoteFile('index.md', path.resolve('README.md'));
