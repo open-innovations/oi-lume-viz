d5f7a4ee60591c82e1624b8c3941d253b0214d4f
Giles Dring <giles@dringtech.com>
2023-02-25 21:14:10 +0000

    Fix test cases

--- components/chart/bar.test.ts
+++ components/chart/bar.test.ts
@@ -1,17 +1,43 @@
-import { describe, it } from "std/testing/bdd.ts";
+import { beforeEach, describe, it } from "std/testing/bdd.ts";
 import { assertThrows } from "std/testing/asserts.ts";
 import { parse as yamlParse } from "std/encoding/yaml.ts";
 import bar, { BarChartOptions } from "./bar.ts";
 
 const testData = yamlParse(
   await Deno.readTextFile("./test/data/bar-chart.yml"),
-) as BarChartOptions[];
+) as {
+  config: BarChartOptions;
+}[];
 
 describe("barChart", () => {
-  testData.forEach((config, i) => {
+  let context: Record<string, unknown>;
+  beforeEach(() => {
+    context = {
+      test: {
+        data: {
+          bar: [
+            {
+              category: "First catergory",
+              value: 10,
+            },
+            {
+              category: "Second category",
+              value: 33,
+            },
+            {
+              category: "Third category",
+              value: 25,
+            },
+          ],
+        },
+      },
+    };
+  });
+  testData.forEach((testCase, i) => {
     it(`should load with test data derived from YFF (config ${i + 1})`, () => {
       bar({
-        config,
+        ...context,
+        config: testCase.config,
       });
     });
   });
--- mod.test.ts
+++ mod.test.ts
@@ -1,89 +1,109 @@
-import Site from 'lume/core/site.ts';
-import { dirname } from 'std/path/mod.ts';
-import { afterEach, beforeEach, describe, it } from 'std/testing/bdd.ts';
+import Site from "lume/core/site.ts";
+import { dirname } from "std/path/mod.ts";
+import { afterEach, beforeEach, describe, it } from "std/testing/bdd.ts";
 import {
   assertSpyCallArg,
   assertSpyCalls,
   Stub,
   stub,
-} from 'std/testing/mock.ts';
+} from "std/testing/mock.ts";
 // TODO mock this call out
-import { assets, components } from './config.ts';
+import { assets, components } from "./config.ts";
 
-import theModule from './mod.ts';
+import theModule from "./mod.ts";
 
 const baseUrl = dirname(import.meta.url);
 
-describe('module', () => {
+describe("module", () => {
   let fakeSite: Site;
   let stubs: Stub[];
 
   beforeEach(() => {
     fakeSite = new Site();
-    stubs = [stub(fakeSite, 'remoteFile'), stub(fakeSite, 'copy')];
+    stubs = [stub(fakeSite, "remoteFile"), stub(fakeSite, "copy")];
   });
 
   afterEach(() => {
     stubs.forEach((s) => s.restore());
   });
 
-  it('should load remote files', () => {
+  it("should load remote files", () => {
     theModule()(fakeSite);
 
     // TODO fix fragile test
-    assertSpyCalls(<Stub>fakeSite.remoteFile, 6);
-    assertSpyCallArg(<Stub>fakeSite.remoteFile, 0, 0, `/assets/${assets[0]}`);
+    assertSpyCalls(<Stub> fakeSite.remoteFile, 8);
+    assertSpyCallArg(<Stub> fakeSite.remoteFile, 0, 0, `/assets/${assets[0]}`);
     assertSpyCallArg(
-      <Stub>fakeSite.remoteFile,
+      <Stub> fakeSite.remoteFile,
       0,
       1,
       `${baseUrl}/assets/${assets[0]}`,
     );
   });
 
-  it('should copy each file separately', () => {
+  it("should copy each file separately", () => {
     theModule()(fakeSite);
-    
+
     // TODO fix fragile test
-    assertSpyCalls(<Stub>fakeSite.copy, 4);
-    assertSpyCallArg(<Stub>fakeSite.copy, 0, 0, `/assets/${assets[0]}`);
+    assertSpyCalls(<Stub> fakeSite.copy, 4);
+    assertSpyCallArg(<Stub> fakeSite.copy, 0, 0, `/assets/${assets[0]}`);
   });
 
-  it('should allow asset path to be set', () => {
+  it("should allow asset path to be set", () => {
     theModule({
-      assetPath: '/fakePath',
+      assetPath: "/fakePath",
     })(fakeSite);
 
-    assertSpyCallArg(<Stub>fakeSite.remoteFile, 0, 0, `/fakePath/${assets[0]}`);
     assertSpyCallArg(
-      <Stub>fakeSite.remoteFile,
+      <Stub> fakeSite.remoteFile,
+      0,
+      0,
+      `/fakePath/${assets[0]}`,
+    );
+    assertSpyCallArg(
+      <Stub> fakeSite.remoteFile,
       0,
       1,
       `${baseUrl}/assets/${assets[0]}`,
     );
-    assertSpyCallArg(<Stub>fakeSite.copy, 0, 0, `/fakePath/${assets[0]}`);
+    assertSpyCallArg(<Stub> fakeSite.copy, 0, 0, `/fakePath/${assets[0]}`);
   });
 
-  it('should allow remove trailing slashes from the asset path to be set', () => {
+  it("should allow remove trailing slashes from the asset path to be set", () => {
     theModule({
-      assetPath: '/fakePath////',
+      assetPath: "/fakePath////",
     })(fakeSite);
 
-    assertSpyCallArg(<Stub>fakeSite.remoteFile, 0, 0, `/fakePath/${assets[0]}`);
-    assertSpyCallArg(<Stub>fakeSite.copy, 0, 0, `/fakePath/${assets[0]}`);
+    assertSpyCallArg(
+      <Stub> fakeSite.remoteFile,
+      0,
+      0,
+      `/fakePath/${assets[0]}`,
+    );
+    assertSpyCallArg(<Stub> fakeSite.copy, 0, 0, `/fakePath/${assets[0]}`);
   });
 
-  it('should default the component namespace to `oi`', () => {
+  it("should default the component namespace to `oi`", () => {
     theModule()(fakeSite);
     // This is a bit fragile
-    assertSpyCallArg(<Stub>fakeSite.remoteFile, assets.length, 0, `_components/oi/${components[0]}`);
+    assertSpyCallArg(
+      <Stub> fakeSite.remoteFile,
+      assets.length,
+      0,
+      `_components/oi/${components[0]}`,
+    );
   });
 
-  it('should allow the component namespace to be overriden `oi.components`', () => {
+  it("should allow the component namespace to be overriden `oi.components`", () => {
     theModule({
-      componentNamespace: 'oi.components'
+      componentNamespace: "oi.components",
     })(fakeSite);
     // This is a bit fragile
-    assertSpyCallArg(<Stub>fakeSite.remoteFile, assets.length, 0, `_components/oi/components/${components[0]}`);
+    assertSpyCallArg(
+      <Stub> fakeSite.remoteFile,
+      assets.length,
+      0,
+      `_components/oi/components/${components[0]}`,
+    );
   });
 });
