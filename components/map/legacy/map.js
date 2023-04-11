eb95b8e0fc32188750708ea872e209227d27e12e
slowe <299787+slowe@users.noreply.github.com>
2023-04-11 13:07:42 +0100

    Fix aspect ratio

--- components/map/legacy/map.js
+++ components/map/legacy/map.js
@@ -689,7 +689,7 @@ function BasicMap(config,attr){
 
 	// Add the SVG
 	this.svg = svgEl('svg');
-	setAttr(this.svg,{'class':'map-inner','xmlns':'http://www.w3.org/2000/svg','version':'1.1','width':this.w,'height':this.h,'viewBox':'-180 0 360 180','overflow':'hidden','style':'max-width:100%;max-height:100%;height:auto;background:'+(attr.background||"white"),'preserveAspectRatio':'xMidYMin meet'});
+	setAttr(this.svg,{'class':'map-inner','xmlns':'http://www.w3.org/2000/svg','version':'1.1','width':this.w,'height':this.h,'viewBox':'-180 0 360 180','overflow':'hidden','style':'max-width:100%;max-height:100%;height:auto;background:'+(attr.background||"white")+';aspect-ratio:'+this.w+' / '+this.h+';','preserveAspectRatio':'xMidYMin meet'});
 	if(config['data-type']) setAttr(this.svg,{'data-type':config['data-type']});
 	el.appendChild(this.svg);
 
