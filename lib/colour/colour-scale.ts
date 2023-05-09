07f26db707d66a1568c7d309c2f86fa719c09f9b
slowe <299787+slowe@users.noreply.github.com>
2023-05-09 15:31:20 +0100

    Add documentation for custom colour scales #21

--- lib/colour/colour-scale.ts
+++ lib/colour/colour-scale.ts
@@ -29,20 +29,23 @@ const namedColourScales: Record<string, string> = {
  * @param scale The CSS code of the new scale
  */
 export function updateColourScales(key: string, scale: string) {
-  namedColourScales[key] = scale;
+	namedColourScales[key] = scale;
+	if(!namedColourScales[key+"_r"]){
+		namedColourScales[key+"_r"] = reverseColourScale(namedColourScales[key]);
+	}
 }
 
 export function getColourScales(): Record<string, string> {
-  return namedColourScales;
+	return namedColourScales;
 }
 
 export function getColourScale(key: string): string {
-  try {
-    return namedColourScales[key];
-  } catch(e) {
-    console.error(e.message);
-    throw new Error('Invalid colour scale requested: ' + key);
-  }
+	try {
+		return namedColourScales[key];
+	} catch(e) {
+		console.error(e.message);
+		throw new Error('Invalid colour scale requested: ' + key);
+	}
 }
 
 // Reverse a colour scale
@@ -71,81 +74,81 @@ for(let s = 0; s < scales.length; s++){
 
 
 export interface ColourScale {
-  (value: number): string;
-  orig: string;
-  gradient: string;
+	(value: number): string;
+	orig: string;
+	gradient: string;
 }
 
 /**
  * A ColourScale function can be created with:
  *
- *   ColourScale('hsl(87, 57%, 86%) 0%, hsl(191, 57%, 15%) 100%')
+ *	 ColourScale('hsl(87, 57%, 86%) 0%, hsl(191, 57%, 15%) 100%')
  *
  * The ColourScale object will, by default, return a colour given a
  * value in the range 0 to 1. It is also possible to return:
  *
- *   ColourScale.orig - the original colour stops string
- *   ColourScale.gradient - a string for the CSS linear gradient
+ *	 ColourScale.orig - the original colour stops string
+ *	 ColourScale.gradient - a string for the CSS linear gradient
  */
 export function ColourScale(gradient: string): ColourScale {
-  const min = 0;
-  const max = 1;
-  if(namedColourScales[gradient]) gradient = namedColourScales[gradient];
-  const stops = extractColours(gradient);
+	const min = 0;
+	const max = 1;
+	if(namedColourScales[gradient]) gradient = namedColourScales[gradient];
+	const stops = extractColours(gradient);
 
-  function getColour(v: number) {
-    const v2 = 100 * (v - min) / (max - min);
-    let cfinal: {
-      r?: number;
-      g?: number;
-      b?: number;
-      alpha?: number;
-    } = {};
-    if (v == max) {
-      cfinal = {
-        "r": stops[stops.length - 1].c.rgb[0],
-        "g": stops[stops.length - 1].c.rgb[1],
-        "b": stops[stops.length - 1].c.rgb[2],
-        "alpha": stops[stops.length - 1].c.alpha,
-      };
-    } else {
-      if (stops.length == 1) {
-        cfinal = {
-          "r": stops[0].c.rgb[0],
-          "g": stops[0].c.rgb[1],
-          "b": stops[0].c.rgb[2],
-          "alpha": parseFloat((v2 / 100).toFixed(3)),
-        };
-      } else {
-        for (let c = 0; c < stops.length - 1; c++) {
-          if (v2 >= stops[c].v && v2 <= stops[c + 1].v) {
-            // On this colour stop
-            let pc = 100 * (v2 - stops[c].v) / (stops[c + 1].v - stops[c].v);
-            if (pc > 100) pc = 100; // Don't go above colour range
-            cfinal = getColourPercent(pc, stops[c].c, stops[c + 1].c);
-            continue;
-          }
-        }
-      }
-    }
+	function getColour(v: number) {
+		const v2 = 100 * (v - min) / (max - min);
+		let cfinal: {
+			r?: number;
+			g?: number;
+			b?: number;
+			alpha?: number;
+		} = {};
+		if (v == max) {
+			cfinal = {
+				"r": stops[stops.length - 1].c.rgb[0],
+				"g": stops[stops.length - 1].c.rgb[1],
+				"b": stops[stops.length - 1].c.rgb[2],
+				"alpha": stops[stops.length - 1].c.alpha,
+			};
+		} else {
+			if (stops.length == 1) {
+				cfinal = {
+					"r": stops[0].c.rgb[0],
+					"g": stops[0].c.rgb[1],
+					"b": stops[0].c.rgb[2],
+					"alpha": parseFloat((v2 / 100).toFixed(3)),
+				};
+			} else {
+				for (let c = 0; c < stops.length - 1; c++) {
+					if (v2 >= stops[c].v && v2 <= stops[c + 1].v) {
+						// On this colour stop
+						let pc = 100 * (v2 - stops[c].v) / (stops[c + 1].v - stops[c].v);
+						if (pc > 100) pc = 100; // Don't go above colour range
+						cfinal = getColourPercent(pc, stops[c].c, stops[c + 1].c);
+						continue;
+					}
+				}
+			}
+		}
 
-    // If no red value is set and the value is greater than the max value, we'll default to the max colour
-    if (typeof cfinal.r !== "number" && v > max) {
-      cfinal = {
-        "r": stops[stops.length - 1].c.rgb[0],
-        "g": stops[stops.length - 1].c.rgb[1],
-        "b": stops[stops.length - 1].c.rgb[2],
-        "alpha": stops[stops.length - 1].c.alpha,
-      };
-    }
+		// If no red value is set and the value is greater than the max value, we'll default to the max colour
+		if (typeof cfinal.r !== "number" && v > max) {
+			cfinal = {
+				"r": stops[stops.length - 1].c.rgb[0],
+				"g": stops[stops.length - 1].c.rgb[1],
+				"b": stops[stops.length - 1].c.rgb[2],
+				"alpha": stops[stops.length - 1].c.alpha,
+			};
+		}
 
-    return "rgba(" + Math.round(cfinal.r) + "," + Math.round(cfinal.g) + "," + Math.round(cfinal.b) + "," +
-      (typeof cfinal.alpha==="number" ? cfinal.alpha : 1) + ")";
-  }
-  getColour.orig = gradient;
-  getColour.gradient = "background: -moz-linear-gradient(left, " + gradient +
-    ");background: -webkit-linear-gradient(left, " + gradient +
-    ");background: linear-gradient(to right, " + gradient + ");";
+		return "rgba(" + Math.round(cfinal.r) + "," + Math.round(cfinal.g) + "," + Math.round(cfinal.b) + "," +
+			(typeof cfinal.alpha==="number" ? cfinal.alpha : 1) + ")";
+	}
+	getColour.orig = gradient;
+	getColour.gradient = "background: -moz-linear-gradient(left, " + gradient +
+		");background: -webkit-linear-gradient(left, " + gradient +
+		");background: linear-gradient(to right, " + gradient + ");";
 
-  return getColour;
+	return getColour;
 }
