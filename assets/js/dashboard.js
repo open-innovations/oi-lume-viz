996cb9c972528fd286c9e46416518e3b41bfe13c
slowe <299787+slowe@users.noreply.github.com>
2023-06-15 11:59:42 +0100

    Generalise code for detecting if in view

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,5 +1,5 @@
 /*
-	Open Innovations Dashboard Interactivity v0.1
+	Open Innovations Dashboard Interactivity v0.2
 	Adds "animation" to numbers in ".oi-dashboard" elements of the form:
 	<div class="oi-dashboard">
 		<div class="panel">
@@ -7,7 +7,6 @@
 		</div>
 	</div>
 */
-
 (function(root){
 
 	if(!root.OI) root.OI = {};
@@ -24,6 +23,86 @@
 		window.requestAnimFrame = (function(){
 			return  window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function( callback ){ window.setTimeout(callback, 1000 / 60); };
 		})();
+	}
+
+	function InView(els){
+		var areas = [];
+		var callback = {};
+
+		// Add events (mouseover, mouseout, click)	
+		this.on = function(type,prop,fn){
+			if(typeof prop==="function" && !fn){
+				fn = prop;
+				prop = "";
+			}
+			if(typeof fn !== "function") return this;
+			if(!callback) callback = {};
+			if(!callback[type]) callback[type] = [];
+			callback[type].push({ 'fn': fn, 'attr': prop });
+			return this;
+		};
+
+		// Trigger a defined event with arguments. This is meant for internal use
+		this.trigger = function(ev,args){
+			if(typeof ev!=="string") return;
+			if(typeof args!=="object") args = {};
+			var i;
+			var o = [];
+			var ths = args['this']||args;
+			if(typeof callback[ev]==="object"){
+				for(i = 0 ; i < callback[ev].length ; i++){
+					if(typeof callback[ev][i].fn==="function"){
+						args.data = callback[ev][i].attr || {};
+						o.push(callback[ev][i].fn.call(ths,args));
+					}
+				}
+			}
+			if(o.length > 0) return o;
+		};
+
+		this.add = function(els){
+			var ok,i,a;
+			for(i = 0; i < els.length; i++){
+				ok = true;
+				for(a = 0; a < areas.length; a++){
+					if(areas[a].el==els[i]){ ok = false; continue; }
+				}
+				// Only add an element if we don't already have it
+				if(ok) areas.push({'el':els[i],'activated':false,'visible':false});
+			}
+			return this;
+		};
+
+		// Check if the element is within the viewport
+		function inViewport(el) {
+			var b,w,h;
+			b = el.getBoundingClientRect();
+			h = el.offsetHeight;
+			w = el.offsetWidth;
+			return (b.top >= -h && b.left >= -w && b.right <= (window.innerWidth || document.documentElement.clientWidth) + w && b.bottom <= (window.innerHeight || document.documentElement.clientHeight) + h);
+		}
+
+		var _obj = this;
+
+		function scrolled(){
+			var a,ok;
+			for(a = 0; a < areas.length; a++){
+				ok = inViewport(areas[a].el);
+				if(ok && !areas[a].activated){
+					areas[a].activated = true;
+					_obj.trigger('activate',areas[a]);
+				}
+				if(ok && !areas[a].visible) _obj.trigger('enter',areas[a]);
+				if(!ok && areas[a].visible) _obj.trigger('leave',areas[a]);
+				areas[a].visible = ok;
+			}
+		}
+
+		document.addEventListener('scroll',scrolled);
+
+		if(els) this.add(els);	// Add the elements
+		scrolled();	// Trigger the scroll event even though we haven't scrolled
+		return this;
 	}
 
 	function AugmentDashboards(){
@@ -68,30 +147,12 @@
 			if(v > 1e4) return Math.round(v/1e3)+"k";
 			return v;
 		}
-		// Check if the element is within the viewport
-		function inViewport(el) {
-			var b,w,h;
-			b = el.getBoundingClientRect();
-			h = el.offsetHeight;
-			w = el.offsetWidth;
-			return (b.top >= -h && b.left >= -w && b.right <= (window.innerWidth || document.documentElement.clientWidth) + w && b.bottom <= (window.innerHeight || document.documentElement.clientHeight) + h);
-		}
-		// For each dashboard, check if "activated" is set. If not, animate the panels and set the attribute.
-		function activateDashboards(){
-			var d,p,panels;
-			for(d = 0; d < dashboards.length; d++){
-				if(!dashboards[d].getAttribute('activated') && inViewport(dashboards[d])){
-					dashboards[d].setAttribute('activated',true);
-					// Now trigger the number animations
-					panels = dashboards[d].querySelectorAll('.bignum');
-					for(p = 0; p < panels.length; p++) animateNumber(panels[p]);
-				}
-			}
-		}
-		// Attach the scroll event
-		document.addEventListener('scroll', activateDashboards);
-		// See if any dashboards need to be activated
-		activateDashboards();
+		var monitor = new InView(dashboards);
+		monitor.on('activate',{'test':'blah'},function(e){
+			// Trigger the number animations on the panels
+			var panels = e.el.querySelectorAll('.bignum');
+			for(var p = 0; p < panels.length; p++) animateNumber(panels[p]);
+		});
 	}
 	root.OI.AugmentDashboards = AugmentDashboards;
 
