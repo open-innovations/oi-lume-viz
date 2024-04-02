da85e3040ca2f34fa4438e5ddfef9784a2d3865a
slowe <299787+slowe@users.noreply.github.com>
2024-04-02 18:02:28 +0100

    Return default background when parse colour fails

--- lib/colour/parse-colour-string.ts
+++ lib/colour/parse-colour-string.ts
@@ -40,6 +40,7 @@ export function parseColourString(
 	input = replaceNamedColours(input);
 	let expectedLength = 3;
 	let maxValues = [255, 255, 255];
+	let rgb,hsl,hex;
 	if (input.toLowerCase().indexOf("hsl") == 0) {
 		const str = input.replace(/hsla?\(/i, "").replace(/\)/, "");
 		maxValues = [360, 100, 100];
@@ -47,17 +48,16 @@ export function parseColourString(
 			expectedLength = 4;
 			maxValues.push(1);
 		}
-		const hsl = splitStringToNumbers(str);
+		hsl = splitStringToNumbers(str);
 		validateNumberList(hsl, {
 			expectedLength: expectedLength,
 			maxValues: maxValues,
 		});
-		const rgb = hslToRgb(hsl[0], hsl[1], hsl[2]);
-		const hex = hslToHex(hsl[0], hsl[1], hsl[2]);
-		return { rgb, hex, hsl };
+		rgb = hslToRgb(hsl[0], hsl[1], hsl[2]);
+		hex = hslToHex(hsl[0], hsl[1], hsl[2]);
 	}else if (input.toLowerCase().indexOf("rgb") == 0) {
 		const str = input.replace(/rgba?\(/i, "").replace(/\)/, "");
-		const rgb = splitStringToNumbers(str);
+		rgb = splitStringToNumbers(str);
 		if (input[3].toLowerCase() === "a") {
 			expectedLength = 4;
 			maxValues.push(1);
@@ -66,16 +66,16 @@ export function parseColourString(
 			expectedLength: expectedLength,
 			maxValues: maxValues,
 		});
-		const hsl = rgbToHsl(rgb[0], rgb[1], rgb[2]);
-		const hex = rgbToHex(rgb[0], rgb[1], rgb[2]);
-		return { rgb, hex, hsl };
+		hsl = rgbToHsl(rgb[0], rgb[1], rgb[2]);
+		hex = rgbToHex(rgb[0], rgb[1], rgb[2]);
 	}else if (input.indexOf("#") == 0) {
-		const hex = input;
-		const rgb = hexToRGB(hex);
-		const hsl = rgbToHsl(rgb[0], rgb[1], rgb[2]);
-		return { rgb, hex, hsl };
+		hex = input;
+		rgb = hexToRGB(hex);
+		hsl = rgbToHsl(rgb[0], rgb[1], rgb[2]);
 	}else{
-		console.log('Attempting to parse '+input);
+		console.warn('Unable to parse colour string "'+input+'"');
+		// Return default background colour
+		return parseColourString(getBackgroundColour());
 	}
-	throw "Unable to parse colour string";
+	return { rgb, hex, hsl };
 }
