a9ec3cc12ac0990b0a207e858dca9d15f68a7ecb
slowe <299787+slowe@users.noreply.github.com>
2024-07-10 18:39:19 +0100

    Add `strptime` and `decimalYear`

--- assets/js/map-slider.js
+++ assets/js/map-slider.js
@@ -181,7 +181,7 @@
 		}
 
 		// For each {{ value }} we will parse it to see if we recognise it
-		value = value.replace(/\{\{ *([^\}]*) *\}\}/g,function(m,p1){
+		value = value.replace(/\{\{ *(.*?) *\}\}/g,function(m,p1){
 
 			// Remove a trailing space
 			p1 = p1.replace(/ $/g,"");
@@ -270,6 +270,30 @@
 					bits[b] = "";
 				}
 
+				// strptime("%Y-%m-%d")
+				rtn = bits[b].match(/strptime\([\"\']([^\)]*)[\"\']\)/);
+				if(p1 && rtn){
+					p1 = strptime(p1||"%Y-%m-%d",rtn[1]);
+					bits[b] = "";
+				}
+
+				// decimalYear()
+				rtn = bits[b].match(/decimalYear\(([^\)]*)\)/);
+				if(p1 && rtn){
+					var sy = new Date(p1);
+					// Start of year
+					sy.setUTCMonth(0);
+					sy.setUTCDate(1);
+					sy.setUTCHours(0);
+					sy.setUTCMinutes(0);
+					sy.setUTCSeconds(0);
+					// Start of next year
+					var ey = new Date(sy);
+					ey.setUTCFullYear(sy.getUTCFullYear()+1);
+					p1 = sy.getUTCFullYear() + (p1-sy)/(ey-sy);
+					bits[b] = "";
+				}
+
 				// colourScale(scale,min,max)
 				rtn = bits[b].match(/colourScale\([\"\']([^\"\']+)[\"\']([^\)]*)\)/);
 				if(p1 && rtn){
@@ -307,6 +331,100 @@
 		});
 
 		return value;
+	}
+	
+	// strptime() - parses a string that matches a given format https://www.tutorialspoint.com/posix-function-strftime-in-perl
+	function strptime(datestr,format){
+
+		var monthshort = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+		var monthlong = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+		var d = {'y':1970,'m':1,'d':1,'hh':0,'mm':0,'ss':0};
+
+		/* Expand aliases */
+		format = format.replace(/%D/, "%m/%d/%y");
+		format = format.replace(/%F/, "%Y-%m-%d");
+		format = format.replace(/%R/, "%H:%M");
+		format = format.replace(/%T/, "%H:%M:%S");
+
+		if(!format.match(/\%[a-zA-Z]/)) return datestr;
+
+		var i;
+		var parts = format.split(/\%/g);
+		var pattern = "";
+		
+		for(i = 0; i < parts.length; i++){
+			if(i==0 && format[0]!="%") parts[i] = {'c':'','post':parts[i]};		else parts[i] = {'c':(parts[i].length > 0 ? parts[i][0]:''),'post':parts[i].substr(1,)};
+
+			switch(parts[i].c){
+				case '%':
+					parts[i].pattern = '%';
+					break;
+				case 'y':
+					parts[i].pattern = "([0-9]{2})";
+					parts[i].fn = function(a,d){ d.y = 2000+parseInt(a); };
+					break;
+				case 'Y':
+					parts[i].pattern = "([0-9]{4})";
+					parts[i].fn = function(a,d){ d.y = parseInt(a); };
+					break;
+				case 'b':
+				case 'h':
+					parts[i].pattern = "(" + monthshort.join("|") + ")";
+					parts[i].fn = function(a,d){ d.m = monthshort.indexOf(a)+1; };
+					break;
+				case 'B':
+					parts[i].pattern = "(" + monthlong.join("|") + ")";
+					parts[i].fn = function(a,d){ d.m = monthlong.indexOf(a)+1; };
+					break;
+				case 'm':
+					parts[i].pattern = "([0-9]{2})";
+					parts[i].fn = function(a,d){ d.m = parseInt(a); };
+					break;
+				case 'd':
+					parts[i].pattern = "([0-9]{2})";
+					parts[i].fn = function(a,d){ d.d = parseInt(a); };
+					break;
+				case 'e':
+					parts[i].pattern = "([0-9]{1,2})";
+					parts[i].fn = function(a,d){ d.d = parseInt(a); };
+					break;
+				case 'H':
+				case 'I':
+					parts[i].pattern = "([0-9]{1,2})";
+					parts[i].fn = function(a,d){ d.hh = parseInt(a); };
+					break;
+				case 'M':
+					parts[i].pattern = "([0-9]{1,2})";
+					parts[i].fn = function(a,d){ d.mm = parseInt(a); };
+					break;
+				case 'S':
+					parts[i].pattern = "([0-9]{1,2})";
+					parts[i].fn = function(a,d){ d.ss = parseInt(a); };
+					break;
+				case 'p':
+					parts[i].pattern = "(a\.?m\.?|p\.?\m\.?)";
+					parts[i].fn = function(a,d){ if(a.replace(/\./g,'').toLowerCase()=="pm" && d.hh < 12){ d.hh += 12; }  };
+					break;
+			}
+			if(parts[i].pattern) pattern += parts[i].pattern;
+			pattern += parts[i].post;
+		}
+		var datebits = datestr.match(RegExp(pattern,"i"));
+		if(datebits){
+			var j = 1;
+			for(i = 0; i < parts.length; i++){
+				if(parts[i].pattern){
+					if(typeof parts[i].fn==="function"){
+						parts[i].fn.call(this,datebits[j],d);
+					}
+					j++;
+				}
+			}
+		}else{
+			// Bad match
+			console.warn('Bad match for "%c'+datestr+'%c" compared to "%c'+pattern+'%c"','font-style:italic;','','font-style:italic;','');
+		}
+		return new Date(d.y+'-'+(d.m<10 ? '0':'')+d.m+'-'+(d.d<10 ? '0':'')+d.d+'T'+(d.hh<10 ? '0':'')+d.hh+':'+(d.mm<10 ? '0':'')+d.mm+':'+(d.ss<10 ? '0':'')+d.ss+'Z');
 	}
 	root.OI.SliderMap = function(opt){
 		var p = document.currentScript.parentNode;
