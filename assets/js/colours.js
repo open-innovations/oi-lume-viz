444359cf8920fa34c8eb61ce4d8d0c76561f3f16
slowe <299787+slowe@users.noreply.github.com>
2024-10-09 09:57:25 +0100

    Trim colour contrast code

--- assets/js/colours.js
+++ assets/js/colours.js
@@ -274,55 +274,14 @@
 		return getColour;
 	}
 
-	// Convert to sRGB colorspace
+	// Return the most contrasty colour
 	// https://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
-	function sRGBToLinear(v){
-		v /= 255;
-		if (v <= 0.03928) return v/12.92;
-		else return Math.pow((v+0.055)/1.055,2.4);
-	}
-	// https://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
-	function relativeLuminance(rgb){ return 0.2126 * sRGBToLinear(rgb[0]) + 0.7152 * sRGBToLinear(rgb[1]) + 0.0722 * sRGBToLinear(rgb[2]); }
 	// https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-contrast.html#contrast-ratiodef
-	function contrastRatio(a, b){
-		var L1 = relativeLuminance(a);
-		var L2 = relativeLuminance(b);
-		if(L1 < L2){
-			var temp = L2;
-			L2 = L1;
-			L1 = temp;
-		}
-		return (L1 + 0.05) / (L2 + 0.05);
-	}	
-	function contrastColour(c){
-		var rgb = [];
-		if(c.indexOf('#')==0){
-			rgb = [h2d(c.substring(1,3)),h2d(c.substring(3,5)),h2d(c.substring(5,7))];
-		}else if(c.indexOf('rgb')==0){
-			var bits = c.match(/[0-9\.]+/g);
-			if(bits.length == 4) this.alpha = parseFloat(bits[3]);
-			rgb = [parseInt(bits[0]),parseInt(bits[1]),parseInt(bits[2])];
-		}
-		var cols = {
-			"black": [0, 0, 0],
-			"white": [255, 255, 255],
-		};
-		var maxRatio = 0;
-		var contrast = "white";
-		for(var col in cols){
-			var contr = contrastRatio(rgb, cols[col]);
-			if(contr > maxRatio){
-				maxRatio = contr;
-				contrast = col;
-			}
-		}
-		if(maxRatio < 4.5){
-			console.warn('Text contrast poor ('+maxRatio.toFixed(1)+') for %c'+c+'%c','background:'+c+';color:'+contrast,'background:none;color:inherit;');
-		}else if(maxRatio < 7){
-			//console.warn('Text contrast good ('+maxRatio.toFixed(1)+') for %c'+c+'%c','background:'+c+';color:'+contrast,'background:none;color:inherit;');
-		}
-		return contrast;
-	}
+	function toLin(v){ v /= 255; if (v <= 0.03928){ return v/12.92; }else{ return Math.pow((v+0.055)/1.055,2.4); }}
+	function rLum(rgb){ return 0.2126 * toLin(rgb[0]) + 0.7152 * toLin(rgb[1]) + 0.0722 * toLin(rgb[2]); }
+	function contrastRatio(a, b){ let L1 = rLum(a); let L2 = rLum(b); if(L1 < L2){ let temp = L2; L2 = L1; L1 = temp; } return (L1 + 0.05) / (L2 + 0.05); }
+	function contrastColour(c){ let rgb = [h2d(c.substring(1,3)),h2d(c.substring(3,5)),h2d(c.substring(5,7))]; return (cRatio(rgb,[0, 0, 0]) > contrastRatio(rgb,[255, 255, 255]) ? "black" : "white"); }
+
 	if(!root.OI.contrastColour) root.OI.contrastColour = contrastColour;
 
 	root.OI.ColourScale = ColourScale;
